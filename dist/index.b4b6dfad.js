7805769054113103663471415c573f996dd233b6
SamLCH <64434536+sam-vic@users.noreply.github.com>
2023-06-27 18:13:13 -0700

    Update index.b4b6dfad.js

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -27511,6 +27511,7 @@ var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "DetailCard", ()=>DetailCard);
 var _jsxDevRuntime = require("react/jsx-dev-runtime");
+var _propTypes = require("prop-types");
 var _propTypesDefault = parcelHelpers.interopDefault(_propTypes);
 const DetailCard = ({ movie , onMovieClick  })=>{
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
